07b32d867b3d325af4b2c1aa20c8b75764950575
Zheng Jie <eynsfordcq@gmail.com>
2025-07-22 08:07:25 +0800

    add server connect

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	ampq "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+	connStr := "amqp://guest:guest@localhost:5672/"
+	conn, err := ampq.Dial(connStr)
+	if err != nil {
+		fmt.Println("fail to connect to rabbit mq", err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	fmt.Println("Successfully connected to RabbitMQ.")
+	fmt.Println("  [*] Waiting for messages. To exit, press CTRL+C")
+
+	// create a channel to receive signals
+	signals := make(chan os.Signal, 1)
+	// notify the channel for SIGINT (CTRL+C) and SIGTERM (kill)
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+	// block until a signal is received
+	<-signals
+	fmt.Println("\nShutting down...")
+
 }
