fb54205583cc32701c40976f3455904cae77867e
Zheng Jie <eynsfordcq@gmail.com>
2025-07-24 08:59:29 +0800

    add bind queue

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril client...")
+	connStr := "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(connStr)
+	if err != nil {
+		log.Fatalf("fail to connect to rabbit mq: %v", err)
+	}
+	defer conn.Close()
+
+	fmt.Println("Successfully connected to RabbitMQ.")
+
+	username, err := gamelogic.ClientWelcome()
+	if err != nil {
+		log.Fatalf("fail to get username: %v", err)
+	}
+
+	queueName := fmt.Sprintf("%s.%s", routing.PauseKey, username)
+
+	_, queue, err := pubsub.DeclareAndBind(
+		conn,
+		routing.ExchangePerilDirect,
+		queueName,
+		routing.PauseKey,
+		pubsub.SimpleQueueTransient,
+	)
+	if err != nil {
+		log.Fatalf("fail to bind queue: %v", err)
+	}
+
+	fmt.Printf("Queue '%s' declared and bound to exchange '%s' with routing key '%s'\n",
+		queue.Name,
+		routing.ExchangePerilDirect,
+		routing.PauseKey,
+	)
+
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+	<-signals
+	fmt.Println("\nShutting down...")
+
 }
---
+++ internal/pubsub/consume.go
@@ -0,0 +1,52 @@
+package pubsub
+
+import amqp "github.com/rabbitmq/amqp091-go"
+
+type SimpleQueueType int
+
+const (
+	SimpleQueueDurable SimpleQueueType = iota
+	SimpleQueueTransient
+)
+
+func DeclareAndBind(
+	conn *amqp.Connection,
+	exchange,
+	queueName,
+	key string,
+	queueType SimpleQueueType,
+) (*amqp.Channel, amqp.Queue, error) {
+	ch, err := conn.Channel()
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+
+	durable := queueType == SimpleQueueDurable
+	autoDelete := queueType == SimpleQueueTransient
+	exclusive := queueType == SimpleQueueTransient
+
+	queue, err := ch.QueueDeclare(
+		queueName,
+		durable,
+		autoDelete,
+		exclusive,
+		false,
+		nil,
+	)
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+
+	err = ch.QueueBind(
+		queueName,
+		key,
+		exchange,
+		false,
+		nil,
+	)
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+
+	return ch, queue, nil
+}
